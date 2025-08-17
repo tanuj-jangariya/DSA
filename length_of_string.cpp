22ded3c022e70228dbc1cea7518ad71a28aaf407
tanuj-jangariya <tanuj21112003@gmail.com>
2025-08-17 23:52:16 +0530

    Length_Of_String

---
+++ length_of_string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int getLength(char name[]){
+
+    int count = 0;
+
+    for(int i=0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[30];
+
+    cout<<"Enter your name "<<endl;
+    cin>>name;
+
+    cout<<"your name is ";
+    cout<<name<<endl;
+
+    cout<<"length : " <<getLength(name) <<endl;
+
+    return 0;
+}
