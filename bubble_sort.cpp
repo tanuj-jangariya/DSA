e0fe5d98c3538715c21bfbadb114d4d04a50f73e
tanuj-jangariya <tanuj21112003@gmail.com>
2025-09-27 23:51:01 +0530

    Bubble_Sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubblesort(vector<int>&arr, int n){
+
+    for(int i=1; i<n; i++){
+
+        for(int j=0; j<n-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
