ca88f0e8e7eeab0636a115127bd87ac8f810a4aa
tanuj-jangariya <tanuj21112003@gmail.com>
2025-11-17 23:01:28 +0530

    Search_in_a_2D_matrix

---
+++ Search_in_a_2D_matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size(), m = matrix[0].size();
+
+    //traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
