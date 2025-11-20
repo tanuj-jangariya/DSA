c137c03069bdee9efb33e35645dd3f1b08c448ab
tanuj-jangariya <tanuj21112003@gmail.com>
2025-11-20 22:21:58 +0530

    Isomorphic_String

---
+++ Isomorphic_String.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  class Solution {
+  public:
+      bool isomorphicString(string s, string t) {
+          int m1[256] = {0}, m2[256] = {0}; 
+          
+          int n = s.size(); 
+          
+          for (int i = 0; i < n; ++i) {
+              if (m1[s[i]] != m2[t[i]]) return false;
+              
+              m1[s[i]] = i + 1;
+              m2[t[i]] = i + 1;
+          }
+          
+          return true;
+      }
+  };
+  
+  int main() {
+      Solution solution;
+      
+      string s = "paper";
+      string t = "title";
+      
+      if (solution.isomorphicString(s, t)) {
+          cout << "Strings are isomorphic." << endl;
+      } else {
+          cout << "Strings are not isomorphic." << endl;
+      }
+      
+      return 0;
+  }
